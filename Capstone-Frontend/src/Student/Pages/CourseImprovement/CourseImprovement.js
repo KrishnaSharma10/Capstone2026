dd2c83c3a7c7efb2a8f5004a4ee28e3832b3fd2b
KrishnaSharma10 <ksharma10_be23@thapar.edu>
2026-08-10 10:31:35 +0530

    test 4

--- Capstone-Frontend/src/Student/Pages/CourseImprovement/CourseImprovement.js
+++ Capstone-Frontend/src/Student/Pages/CourseImprovement/CourseImprovement.js
@@ -281,7 +281,7 @@ const CourseImprovement = () => {
       studentData: student
     };
 
-    axios.post("https://tier-according-comparative-old.trycloudflare.com", data)
+    axios.post("https://comparable-chose-complimentary-justice.trycloudflare.com", data)
       .then((res) => {
         setChoices(res.data.choices);
         setNewTimeTable(res.data.newTimeTable);
